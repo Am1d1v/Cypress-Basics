db536c04ebde09215ee0bc5927d11135ad6e7b00
Am1d1v <dimadiman1adima@gmail.com>
2024-06-14 12:41:00 +0300

    Basic Project Setup

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,11 +1,7 @@
 describe('template spec', () => {
 
   it('passes', () => {
-    // Visit a page
     cy.visit('http://localhost:5173/');
-
-    // Search for a certain element
-    cy.get('li').should('have.length', 6);
   })
 
 })
