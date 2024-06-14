c6649b519869c4994d64b478905f48a85a23f347
Am1d1v <dimadiman1adima@gmail.com>
2024-06-14 19:58:45 +0300

    Open/Close add new task.

--- cypress/e2e/Tasks.cy.js
+++ cypress/e2e/Tasks.cy.js
@@ -2,14 +2,30 @@
 
 describe('Tasks management', () => {
 
-    it('should open and close the new task model', () => {
+    it('should open and close the new task model by clicking on "Cancel" button', () => {
         cy.visit('http://localhost:5173/');
 
         // Check that the "Add Task" button exists
         cy.get('button').contains('Add Task');
 
-        // Click on the "Add Task" button
+        // Open modal by clicking on the "Add Task" button
         cy.contains('Add Task').click();
+
+        // Close modal by clicking on the "Cancel" button
+        cy.contains('Cancel').click();
+
+        
+    });
+
+    it('should open and close the new task model by clicking on the backdrop', () => {
+        cy.visit('http://localhost:5173/');
+
+        // Check that the "Add Task" button exists
+        cy.get('button').contains('Add Task');
+
+        // Close modal by clicking on the backdrop
+        cy.contains('Add Task').click();
+        cy.get('.backdrop').click({force: true});
         
     });
 
