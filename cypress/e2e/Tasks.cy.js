ae1d20dc865adb4c93af6d191d550e5f48c67297
Am1d1v <dimadiman1adima@gmail.com>
2024-06-15 14:35:56 +0300

    Selecting Dropdown values. Task filter

--- cypress/e2e/Tasks.cy.js
+++ cypress/e2e/Tasks.cy.js
@@ -87,4 +87,41 @@ describe('Tasks management', () => {
         cy.get("dialog.modal p.error-message").contains('Please provide values for task title, summary and category!');
     });
 
+    it.only('should filter tasks', () => {
+        cy.visit('http://localhost:5173/');
+
+        // Open modal window
+        cy.contains('Add Task').click();
+
+        // Select title input and then type data into it
+        cy.get('#title').type('Title with urgent priority');
+
+        // Select summary input and then type data into it
+        cy.get('#summary').type('Description of urgent information');
+
+        // Click on category and select "urgent" priority
+        cy.get('select#category').select('urgent');
+
+        // Submit task by clicking on "Add Task" button
+        cy.get('dialog.modal').contains('Add Task').click();
+
+        // Select all in category filter
+        cy.get('select#filter').select('all');
+
+        // Check that at least 1 task exists
+        cy.get('.task').should('have.length.above', 0);
+
+        // Select urgent in category filter
+        cy.get('select#filter').select('urgent');
+
+        // Check that creater earlier task exists
+        cy.get('.task').should('have.length.above', 0);
+
+        // Select low in category filter
+        cy.get('select#filter').select('low');
+
+        // Check that the task with urgent priority doesn't appears in others filter categories but 'All'
+        cy.get('.task').should('have.length', 0); 
+    });
+
 });
